868f61b7bf8349ef3dd4917624a885cb84dc6bfc
zmjwkl <3014047624@qq.com>
2024-08-28 10:33:36 +0800

    Add files via upload

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -16,11 +16,24 @@ func ExecuteCommand(action, resource, name, namespace string) (string, error) {
 	return string(output), nil
 }
 
-func GetVcjobStatus(name, namespace string) (string, error) {
-	cmd := exec.Command("kubectl", "get", "vcjob", name, "-n", namespace, "-o", "jsonpath={.status.conditions[0].type}")
+// 查询所有节点状态
+func ExecuteCommand_getnodes() (string, error) {
+	// 示例：使用 kubectl 执行命令
+	cmd := exec.Command("kubectl", "get", "node")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("failed to get vcjob status: %v, output: %s", err, string(output)+" "+cmd.String()+"\n")
+		return "", fmt.Errorf("failed to execute command: %v, output: %s", err, string(output)+" "+cmd.String()+"\n")
+	}
+	return string(output), nil
+}
+
+// 查询具体节点状态
+func ExecuteCommand_getnode(node_name string) (string, error) {
+	// 示例：使用 kubectl 执行命令
+	cmd := exec.Command("kubectl", "discribe", "node", node_name)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("failed to execute command: %v, output: %s", err, string(output)+" "+cmd.String()+"\n")
 	}
 	return string(output), nil
 }
